83491f854993524d9b6ec2613f6a0dddf064d5bd
alfi-teachs <alfiarajab@gmail.com>
2026-07-20 19:35:29 +0530

    Update app.js

---
+++ app/app.js
@@ -0,0 +1,50 @@
+// ======================================
+// Enterprise DevOps Platform
+// app.js
+// ======================================
+
+// Display Current Date & Time
+
+function updateDateTime() {
+
+    const now = new Date();
+
+    document.getElementById("datetime").innerHTML =
+        now.toLocaleString();
+
+}
+
+// Refresh every second
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
+
+
+// Deployment Status
+
+const status = document.getElementById("status");
+
+status.innerHTML = "Running ✅";
+
+status.style.color = "green";
+
+
+// Environment
+
+const environment = "Development";
+
+console.log("Environment :", environment);
+
+
+// Application Version
+
+const version = "1.0.0";
+
+console.log("Application Version :", version);
+
+
+// Page Loaded
+
+console.log("Enterprise DevOps Platform Loaded Successfully");
+console.log("Loaded at :", new Date().toLocaleString());
